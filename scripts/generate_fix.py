a691d94c79349f2a7ddfb2205d304022b36b33e3
Arjun Bahuguna <18398862+arjunbahuguna@users.noreply.github.com>
2024-04-22 09:40:50 +0530

    Update generate_fix.py
    Update variable names

--- scripts/generate_fix.py
+++ scripts/generate_fix.py
@@ -2,11 +2,11 @@ import torch
 import librosa as li
 import soundfile as sf
 
-x, sr = li.load(“path-to-input-audio.wav", sr=44100)
+x, sr = li.load("input_audio.wav", sr=44100)
 
-model = torch.jit.load("path-to-torchscript-model.ts").eval()
+model = torch.jit.load("model_filename.ts").eval()
 x = torch.from_numpy(x).reshape(1,1,-1)
 z = model.encode(x)
 x_hat = model.decode(z).detach().numpy().reshape(-1)
 
-sf.write("model_output.wav", x_hat, sr)
+sf.write("output_audio.wav", x_hat, sr)
