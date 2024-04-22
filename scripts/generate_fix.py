782b30acdf53fe51388fb003eafe94ab34e4c715
Arjun Bahuguna <18398862+arjunbahuguna@users.noreply.github.com>
2024-04-22 09:39:19 +0530

    Update generate_fix.py
    Fixed typo

--- scripts/generate_fix.py
+++ scripts/generate_fix.py
@@ -4,7 +4,7 @@ import soundfile as sf
 
 x, sr = li.load(“path-to-input-audio.wav", sr=44100)
 
-model = torch.jit.load(“path-to-torchscript-model.ts").eval()
+model = torch.jit.load("path-to-torchscript-model.ts").eval()
 x = torch.from_numpy(x).reshape(1,1,-1)
 z = model.encode(x)
 x_hat = model.decode(z).detach().numpy().reshape(-1)
